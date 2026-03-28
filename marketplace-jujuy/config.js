e0cecf6944706a5035e774fe822920e4399028f5
insertmazza <santinomazza09@gmail.com>
2026-03-28 02:25:36 -0300

    update

---
+++ marketplace-jujuy/config.js
@@ -0,0 +1,5 @@
+window.ENV = {
+  SUPABASE_URL: "https://bpdeektcggmwrngnljho.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwZGVla3RjZ2dtd3JuZ25samhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxNTU0NzIsImV4cCI6MjA4OTczMTQ3Mn0.mJ91daJ5IVFt_Fy1z61dcCq6kZrfjXLoIpQmSqkTKis",
+  UPLOAD_WORKER_URL: "https://marketplace-upload.santinomazza09.workers.dev"
+};
